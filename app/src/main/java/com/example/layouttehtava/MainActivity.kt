4c08429294d9d9df096a5ee66132e98f0a2de90d
mikksam <samuel.mikkonen@edu.savonia.fi>
2019-09-19 20:20:26 +0300

    Layout complete

--- app/src/main/java/com/example/layouttehtava/MainActivity.kt
+++ app/src/main/java/com/example/layouttehtava/MainActivity.kt
@@ -13,6 +13,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    //Tee loppuun, jos ehtii!!!!!!!!!!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
