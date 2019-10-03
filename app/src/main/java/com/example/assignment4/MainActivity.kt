53c27230cbdcc4cab3b542520a8248163880830f
mikksam <samuel.mikkonen@edu.savonia.fi>
2019-10-04 02:06:38 +0300

    Done4today

--- app/src/main/java/com/example/assignment4/MainActivity.kt
+++ app/src/main/java/com/example/assignment4/MainActivity.kt
@@ -10,7 +10,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState) 
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val txt:TextView = findViewById(R.id.textView)
--- app/src/main/java/com/example/assignment4/SplashActivity.kt
+++ app/src/main/java/com/example/assignment4/SplashActivity.kt
@@ -24,5 +24,6 @@ class SplashActivity : AppCompatActivity() {
             }
         }
         background.start()
+
     }
 }
